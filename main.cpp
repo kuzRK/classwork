c0db49b6e23822027b552732793253b988b2782f
kuzRK <sanalsogdanov11188@gmail.com>
2026-09-17 19:59:52 +0300

    parallelize summation

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
   }
 
 private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+  std::chrono::time_point< std::chrono::high_resolution_clock > start_;
 };
 
 using data_t = std::vector< unsigned long long >;
@@ -61,5 +61,50 @@ int main(int argc, char **argv)
   }
 
   data_t values(size, 1);
-  
+  std::vector< double > times;
+  times.reserve(5);
+
+  value_t final_sum = 0;
+  for (size_t measurement = 0; measurement < 5; ++measurement)
+  {
+
+    const size_t block_size = size / thread_count;
+
+    std::vector<std::future< value_t > > futures;
+    futures.reserve(thread_count);
+
+    Clicker cl;
+
+    for (size_t i = 0; i < thread_count; ++i)
+    {
+
+      const size_t begin = i * block_size;
+
+      const size_t end = (i == thread_count - 1) ? size : begin + block_size;
+
+      futures.emplace_back(
+          std::async(
+              std::launch::async,
+              sum_range,
+              std::cref(values),
+              begin,
+              end));
+    }
+
+    value_t sum = 0;
+
+    for (auto &f : futures)
+    {
+      sum += f.get();
+    }
+
+    const double time = cl.millisec();
+
+    times.push_back(time);
+    final_sum = sum;
+  }
+  std::sort(times.begin(), times.end());
+
+  const double median = times[times.size() / 2];
+  std::cout << "median ms: " << median << '\n';
 }
